9de3f9607e36d7478b19cd93d5d9a98693281665
Josh Salomon <41079547+JoshSalomon@users.noreply.github.com>
2021-07-01 14:57:59 +0300

    Create dump_ceph_crush_state.sh
    Dump the pools and crush rules into a text file, so we can compare the differences between states of the system during the process of creating read affinity pools and clients in foe Ceph/RHCS

---
+++ dump_ceph_crush_state.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# vim: set ts=4 sw=4 smartindent autoindent:
+
+base_dir=$(dirname "$0")
+data_dir=$base_dir/data
+
+# shellcheck source=./data/crush-utils.sh
+. "$data_dir"/crush-utils.sh
+
+verbose=0
+
+function usage() {
+    #
+    # This function always exits, it never returns
+    #
+    echo
+    echo "Usage: $0 outout-file-name"
+    echo "  Gather the state of the pools and crush rules."
+    echo 
+    exit 1
+}
+
+if [[ -z $1 ]]; then
+    echo_error "Output file name is mandatory"
+    usage
+fi
+
+ofile=$1
+
+set -o noclobber 
+
+echo " Ceph pools" 2> /dev/null 1> $ofile
+if [[ $? -ne 0 ]]; then
+    echo_error "cannot overwrite existing file '$ofile'"
+    exit
+fi
+
+echo " ==========" >> $ofile
+
+ceph osd pool ls detail >> $ofile
+
+echo "" >> $ofile
+echo " Crush rules" >> $ofile
+echo " ===========" >> $ofile
+
+ceph osd crush rule dump >> $ofile
+
+echo "Output was saved to $ofile"
+
+exit
+
